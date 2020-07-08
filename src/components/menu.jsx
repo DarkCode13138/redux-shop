8ad709b384be3bb3042dded113c7d154d29ef691
CrazyCod <pashatoni21@gmail.com>
2020-07-08 12:57:53 +0300

    deploy

---
+++ src/components/menu.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Menu, Popup, List, Button,Image } from 'semantic-ui-react';
+
+const CartComponent = ({title, id, image, removeFromCart}) => (
+    <List selection divided verticalAlign='middle'>
+        <List.Item>
+            <List.Content floated='right'>
+                <Button onClick={removeFromCart.bind(this, id)} color='red'>Удалить</Button>
+            </List.Content>
+            <Image avatar src={image} />
+            <List.Content>{title}</List.Content>
+        </List.Item>
+    </List>
+)
+
+const MenuComponent = ({totalPrice, count, items}) => {
+    return(
+
+        <Menu>
+            <Menu.Item
+                name='browse'
+
+                onClick={this}
+            >
+                Магазин книг
+            </Menu.Item>
+            <Menu.Menu position='right'>
+                <Menu.Item
+                    name='signup'
+
+                    onClick={this}
+                >
+                    Итого: &nbsp;<b>{totalPrice}</b> руб.
+                </Menu.Item>
+
+
+                <Popup
+                  trigger={ <Menu.Item
+                    name='help'
+
+                    onClick={this}
+                >
+                    Корзина (<b>{count}</b>)
+                </Menu.Item>}
+                    content={items.map(book => <CartComponent{...book}/>)}
+                  on='click'
+                  hideOnScroll
+                />
+
+            </Menu.Menu>
+        </Menu>
+
+    )
+}
+
+export default MenuComponent;
